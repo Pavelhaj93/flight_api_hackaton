4baae645ea31a0015eeb900e280d24f64cc7e41d
Daniel <virusxd521@gmail.com>
2022-03-11 11:06:57 +0200

    finishing the structure

--- src/Components/Wrapper.js
+++ src/Components/Wrapper.js
@@ -4,6 +4,14 @@ const Wrapper = () => {
      [apiData, setApiData] = useState(null);
 
 
+
+    //  Todo if we will have time showing the location of the user 
+
+
+    // YOUR_AFFILID: data4youcbp202106
+    const YOUR_AFFILID = 'data4youcbp202106';
+    const url = `https://api.skypicker.com/flights?fly_from=PRG&partner=${YOUR_AFFILID}`
+
     const fetchingData = async () => {
         const response = await fetch();
         const data = await response.json();
